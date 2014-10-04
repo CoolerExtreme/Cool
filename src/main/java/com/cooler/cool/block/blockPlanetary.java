f839df48d61eb96242eb4b3fa394427b3f588d6f
CoolerExtreme <hestore@hotmail.com>
2014-10-04 05:43:28 +0530

    Cool Mod

--- src/main/java/com/cooler/cool/block/blockPlanetary.java
+++ src/main/java/com/cooler/cool/block/blockPlanetary.java
@@ -1,20 +1,37 @@
 package com.cooler.cool.block;
 
 import com.cooler.cool.tileEntity.tilePlanetary;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import static com.cooler.cool.reference.reference.MOD_ID;
 
 /**
  * Created by Test on 10/2/2014.
  */
 public class blockPlanetary extends blockCool implements ITileEntityProvider
 {
+    public static IIcon icons[] = new IIcon[3];
     public blockPlanetary()
     {
         super();
         this.setBlockName("BlockPlanetary");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(MOD_ID+":"+this.getUnlocalizedName().substring(5));
+        icons[0] = iconRegister.registerIcon(MOD_ID+":modelSun");
+        icons[1] = iconRegister.registerIcon(MOD_ID+":modelEarth");
+        icons[2] = iconRegister.registerIcon(MOD_ID+":modelMoon");
     }
 
     @Override
--- src/main/java/com/cooler/cool/client/renderer/tile/planetaryRenderer.java
+++ src/main/java/com/cooler/cool/client/renderer/tile/planetaryRenderer.java
@@ -2,10 +2,13 @@ package com.cooler.cool.client.renderer.tile;
 
 import static com.cooler.cool.reference.reference.*;
 
-import assets.cool.models.modelTest;
+import com.cooler.cool.block.blockPlanetary;
+import com.cooler.cool.util.objRendering;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.obj.WavefrontObject;
 import org.lwjgl.opengl.GL11;
 
 /**
@@ -13,27 +16,44 @@ import org.lwjgl.opengl.GL11;
  */
 public class planetaryRenderer extends TileEntitySpecialRenderer
 {
-    public static final modelTest model = new modelTest();
+    WavefrontObject modelSun;
+    WavefrontObject modelEarth;
+    WavefrontObject modelMoon;
     private float angle = 0.0f;
+    private float angleMoon = 0.0f;
 
     @Override
     public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float f)
     {
-        this.bindTexture(new ResourceLocation(MOD_ID, "textures/blocks/BlockPlanetary.png"));
+        modelSun = new WavefrontObject(new ResourceLocation(MOD_ID,"models/modelSun.obj"));
+        modelEarth = new WavefrontObject(new ResourceLocation(MOD_ID,"models/modelEarth.obj"));
+        modelMoon = new WavefrontObject(new ResourceLocation(MOD_ID,"models/modelMoon.obj"));
+
         GL11.glPushMatrix();
         GL11.glTranslatef((float) x, (float) y, (float) z);
-        GL11.glTranslatef(0.5f,-0.75f,0.5f);
-        model.Sun.render(0.0625f);
-        angle += f;
+        this.bindTexture(new ResourceLocation(MOD_ID,"textures/blocks/modelSun.png"));
+        modelSun.renderAll();
+
+        angle++;
         if(angle >= 360.0f)angle -= 360.0f;
+
         GL11.glPushMatrix();
         GL11.glRotatef(angle, 0, 1.0f, 0);
-        GL11.glTranslatef(1.0f,0,0);
-        model.Earth.render(0.0625f);
+        GL11.glTranslatef(4.0f,0,0);
+        GL11.glRotatef(-angle, 0, 1.0f, 0);
+        this.bindTexture(new ResourceLocation(MOD_ID,"textures/blocks/modelEarth.png"));
+        modelEarth.renderAll();
+
+        angleMoon += 4;
+        if(angleMoon >= 360.0f)angleMoon -= 360.0f;
+
         GL11.glPushMatrix();
-        GL11.glRotatef(angle, 1.0f, 0, 0);
-        GL11.glTranslatef(0,0.5f,0);
-        model.Moon.render(0.0625f);
+        GL11.glRotatef(40.0f, 1.0f, 0, 1.0f);
+        GL11.glRotatef(angleMoon, 0, 1.0f, 0);
+        GL11.glTranslatef(1.0f, 0, 0);
+        this.bindTexture(new ResourceLocation(MOD_ID,"textures/blocks/modelMoon.png"));
+        modelMoon.renderAll();
+
         GL11.glPopMatrix();
         GL11.glPopMatrix();
         GL11.glPopMatrix();
